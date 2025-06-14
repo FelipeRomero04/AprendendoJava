96c31e2ca3ed5ec2aae4868b25a72361448a7163
Felipe Romero <felipermrcosta@gmail.com>
2025-06-14 19:55:56 -0300

    Initial commit

---
+++ src/Aulas/Aula02.java
@@ -0,0 +1,14 @@
+package Aulas;
+
+public class Aula02 {
+    public static void main(String[] args){
+        byte idade = 18;
+        short numeroPequeno = (int) 2000;
+        int numero = 32000;
+        long numeroGrande = 310000;
+        float salarioFloat = 3200.0F;
+        double salarioDouble = 233344.2D;
+        char caractere = 86; // De acordo com a table ASCII ou Unicode
+
+    }
+}
