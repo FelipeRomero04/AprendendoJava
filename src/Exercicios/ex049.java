7fc6192df6c78c42ba3eeb6f70399e3bff96cdbb
Felipe Romero <felipermrcosta@gmail.com>
2025-07-08 21:19:24 -0300

    treinando mais

--- src/Exercicios/ex049.java
+++ src/Exercicios/ex049.java
@@ -6,23 +6,25 @@ import java.util.List;
 import java.util.Scanner;
 
 public class ex049 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         List<Double> values = new ArrayList<>();
 
         System.out.print("Digite o primeiro número: ");
         double num1 = input.nextDouble();
-
         System.out.print("Digite o segundo número: ");
         double num2 = input.nextDouble();
+        input.nextLine();
 
-        String line;
-        int option;
         values.add(num1);
         values.add(num2);
 
-        while(true){
-            System.out.println("Opções:\n[1] Somar\n[2] Multiplicar\n[3] Maior\n[4]Novos números\n[5]Sair do Programa");
+        String line;
+        int option;
+
+        while (true) {
+            System.out.println("\nMENU:\n[1] Somar\n[2] Multiplicar\n[3] Maior\n[4]Novos números\n[5]Sair do Programa");
+            System.out.print("Opções: ");
             line = input.nextLine();
             try{
                 option = Integer.parseInt(line);
@@ -36,10 +38,10 @@ public class ex049 {
             }
             if(option == 1){
                 double sum = values.stream().mapToDouble(n -> n).sum();
-                System.out.printf("A soma dos números %.1f e %.1f é %.1f\n", num1, num2, sum);
+                System.out.printf("A soma dos números %.1f + %.1f = %.1f\n", num1, num2, sum);
             }else if(option == 2){
                 double product = num1 * num2;
-                System.out.printf("O produto de %.1f e %.1f é %.1f\n", num1, num2, product);
+                System.out.printf("O produto de %.1f x %.1f = %.1f\n", num1, num2, product);
             } else if (option == 3) {
                 double bigger = Collections.max(values);
                 System.out.printf("Entre %.1f e %.1f o maior é %.1f\n", num1, num2, bigger);
@@ -47,5 +49,6 @@ public class ex049 {
 
 
         }
+
     }
 }
